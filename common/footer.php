03f46a93ec61229c5961de9a4958edcd0399702d
imranmalakzai <afghanimran92@gmail.com>
2025-11-03 19:41:31 +0430

    create footer.php file

---
+++ common/footer.php
@@ -0,0 +1,16 @@
+</div>
+</main>
+
+<script src="js/sweetalert2.all.js"></script>
+<script src="js/helper.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/scripts.js"></script>
+<script src="js/jspdf.umd.min.js"></script>
+<script src="js/html2canvas.min.js"></script>
+<script src="util/download.js"></script>
+<script src="util/pdf.js"></script>
+<script src="util/cvs.js"></script>
+<script src="util/language.js"></script>
+</body>
+
+</html>
