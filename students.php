5039272de9d23f5371e9f09f66abca149d332c8f
imranmalakzai <afghanimran92@gmail.com>
2025-11-08 08:19:51 +0430

    Add user-graduate Icon for new user

--- students.php
+++ students.php
@@ -69,8 +69,8 @@ require "common/header.php" ?>
   <div class="modal-dialog modal-lg modal-dialog-centered">
     <div class="modal-content border-0 shadow-lg rounded-4">
       <div class="modal-header bg-purpal text-white">
-        <h5 class="modal-title" id="addStudentModalLabel">
-          <i class="fa-solid fa-user-graduate me-2"></i> Add New Student
+        <h5 class="modal-title">
+          <i class="fa-solid fa-user-graduate me-2"></i> <span id="addStudentModalLabel">Add New Student</span>
         </h5>
         <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
