eb1fee9bbdf3b26484318f292867f28eaa64be39
imranmalakzai <afghanimran92@gmail.com>
2026-02-20 14:47:30 +0430

    fix: correct db config file

--- Admin/login.api.php
+++ Admin/login.api.php
@@ -6,7 +6,7 @@ header("Content-Type: application/json");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $username = $conn->real_escape_string($_POST["username"]);
-  $password = md5($_POST["password"]); // simple hash for now
+  $password = $_POST["password"]; // simple hash for now
 
   $sql = "SELECT * FROM admin WHERE username='$username' AND password='$password'";
   $result = $conn->query($sql);
